99fd0181c195446c26d8bfdc5e12f2347207dbaf
lightjiang(蒋亮) <lightjiang@tencent.com>
2016-10-12 21:25:45 +0800

    [!]

--- imooc/exports/b.js
+++ imooc/exports/b.js
@@ -4,3 +4,6 @@
 var a  = require('./a');
 //a();
  console.log(a);
+console.log("dirname:"+__dirname); //l当前文件所在的目录
+var path = require('path');
+console.log(path.join('asd','/asd'));
---
+++ node_in_action/webServer/staticFileServer.js
@@ -0,0 +1,54 @@
+/**
+ * Created by lightjiang on 2016/10/12.
+ * 静态文本服务器，用来下载html,js,css,image等静态资源
+ */
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
+var url = require('url');
+
+http.createServer(function(req,res){
+    //获取请求静态资源的绝对路径
+    //__dirname这个变量是指当前文件的绝对路径
+    res.setHeader('Content-Type','text/js; charset=utf-8');
+    var filePath = path.join(__dirname,url.parse(req.url).pathname);
+
+    // var stream = fs.createReadStream(filePath);
+    // stream
+    //     .on('data',function(chunk){
+    //
+    //         res.write(chunk);
+    //     })
+    //     .on('end',function () {
+    //         res.end();
+    //     });
+
+    //使用pipe改造
+    //pipe可以把任意一个ReadableStream中的内容流向一个任意WritableStream。
+    //The response implements , but does not inherit the Writable Stream interface;
+    //The request implements the Writable Stream interface ;  they both are EventEmitters
+    // var stream = fs.createReadStream(filePath);
+    // stream.pipe(res);
+
+    //错误处理
+    fs.stat(filePath , function (err , stat) {
+        if(err){
+            if('ENOENT' == err.code){
+                res.statusCode = 404;
+                res.end('Not Found');
+            }else{
+                res.statusCode = 500;
+                res.end('Internet Server Error');
+            }
+        }else{
+            res.setHeader('Content-length',stat.size);
+            var stream = fs.createReadStream(filePath);
+            stream.pipe(res);
+            stream
+                .on('error',function (err) {
+                    res.statusCode = 500;
+                    res.end('Internet Server Error');
+                });
+        }
+    });
+}).listen(3000);
